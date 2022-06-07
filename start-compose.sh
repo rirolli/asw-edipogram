635a0f63d3598a463a9b060eb52aeb76e368a9b8
Riccardo Ungaro <riccardo.ung@outlook.it>
2022-06-07 19:36:53 +0200

    Aggiunto docker file

---
+++ start-compose.sh
@@ -0,0 +1,9 @@
+# compila il progetto e crea i vari JDK
+echo Building project
+gradle build
+
+# avvia il docker compose
+echo Starting docker compose
+docker compose build
+docker compose push
+docker compose up
---
+++ stop-compose.sh
@@ -0,0 +1,3 @@
+echo Halting docker compose
+
+docker compose down
